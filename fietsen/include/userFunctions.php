a790648487f6b2db88c2fcab99eef404bd454ba6
tinusboss <157947+tinusboss@users.noreply.github.com>
2021-06-20 15:08:46 +0200

    was vergeten op te slaan

--- fietsen/include/userFunctions.php
+++ fietsen/include/userFunctions.php
@@ -8,10 +8,26 @@
     function login(){}
 
     function checkUser($username){
-        if($username <> "")}
+        if($username <> ""){
         $conn=dbConnect();
-        $sql = "select * from gebruikers where username" g
+        $sql = "SELECT * FROM gebruikers WHERE username='$username'" 
+        $stmt = $conn->prepare($sql)
+        $stmt->execute();
+        $stmt->setFetchMode (PDO::FETCH_ASSOC);
+        $users = $stmt->fetchAll();
+        foreach($users as $user){
+            if($username == $user ['username']){
+                return true;
+            }else{
+                return false;
+            }
+        }
 
+    }
+    else{
+        return false;
+    }
+}
     function checkUserPassword($username,$password){}
 
     function checkRole($role){}
@@ -20,6 +36,16 @@
 
     }
 
-    function logout(){}
+    function logout(){
+        $_SESSION['login']=false;
+        $_SESSION['username']="";
+        $_SESSION['role']=0;
+        if(!($_SESSION['login']==true;)){
+            echo "u bent ingelogd.";
+        }else{
+            echo "plobleem met uitloggen";
+        }
+        header('Refresh:2; url=index.php');
+    }
 
 ?>
