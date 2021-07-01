0ab3193c353f53ed7f9f68c88c00ea520503ec22
tinusboss <157947+tinusboss@users.noreply.github.com>
2021-07-01 09:53:59 +0200

    login code er in gezet

--- fietsen/include/html/user/login.php
+++ fietsen/include/html/user/login.php
@@ -24,6 +24,53 @@
         <a href="register.php?page=registreren">registreren</a>
     </p>
 </body>
+<?php
+    define('BASEPATH', true);
+    require 'db.php';
+
+    if (isset($_POST['submit'])) {
+        // try {
+        $dsn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+        $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //kijken of niet alles leeg is
+        $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+        $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+
+        $sql = "SELECT id, username, password FROM admin WHERE username = :username";
+        $stmt = $pdo->prepare($sql);
+
+        //Bind value.
+        $stmt->bindValue(':username', $username);
+
+        //Execute.
+        $stmt->execute();
+
+        //Fetch row.
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //of $row fout is 
+        if ($user === false) {
+            echo '<script>alert("Foute gebruikersnaam of wachtwoord")</script>';
+        } else {
+
+            //vergelijk en ontcijfer het wachtwoord
+            $validPassword = password_verify($passwordAttempt, $user['password']);
+
+            //Als $validPassword is TRUE, Is de login gelukt
+            if ($validPassword == 1) {
+                session_start();
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $username;
+                echo '<script>window.location.replace("ingelogd/reken.php");</script>';
+            } else {
+                //$validPassword was FALSE. Wachtwoord die matcht niet
+                echo '<script>alert("invalid gebruikersnaam of wachtwoord")</script>';
+            }
+        }
+    }
+
+    ?>
 
 </html>
 <?php
